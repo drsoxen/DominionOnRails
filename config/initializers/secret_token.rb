2c5bb0271ffe79c13de9a0a7f81eedd7012f2129
Chris <stargateno1fan@Chriss-MacBook.local>
2012-09-12 10:28:45 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dominion::Application.config.secret_token = '3d5d0c707391b9eefe97d4361719093725e649513d8e053e09097713211b249b2d16045dde47c0cd2c441ea7f761cef99dd2e39c3ce6386a52e077c3845cddc6'
